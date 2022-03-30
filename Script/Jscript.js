85043f80936e50e030eee7e8a6528549d0956771
YenyRamirezSalon <102698365+YenyRamirezSalon@users.noreply.github.com>
2022-03-30 15:54:44 -0500

    Add files via upload

---
+++ Script/Jscript.js
@@ -0,0 +1,20 @@
+$('.menu-item').on('click', function () {
+    var elemento = $(this).text()
+    if (elemento == 'RESERVAR') {
+        var url = 'https://wa.me/51952420156/?text=Buen%20dia,%20quiero%20reservar%20una%20cita.'
+        $(this).attr('href', url)
+    }else if (elemento == 'PRODUCTOS'){
+        alert("Estamos trabajando para brindarle una mejor atención, esperamos su comprensión.")
+    }
+    //
+})
+
+$('.iconreservar').on('click', function () {
+    var servicio = $(this).parent().parent().children('.tiposervicio').text()
+    //closest('.tservicios').firstchild('.tiposervicio').text())
+    var url = 'https://wa.me/51952420156/?text=Buen%20dia,%20quiero%20reservar%20una%20cita%20para:%20*' + servicio + '*'
+    $(location).attr('href', url)
+
+})
+
+
